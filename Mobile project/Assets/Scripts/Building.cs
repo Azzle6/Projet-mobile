4b91e115a387986e251aa31a0f3ab0e5dfbc9c9c
Eliott <eliott.chauchard@gmail.com>
2022-02-08 20:27:52 +0100

    Gitignore + début scripts construction

---
+++ Mobile project/Assets/Scripts/Building.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Building : MonoBehaviour
+{
+    public bool Placed { get; private set; }
+    public BoundsInt area;
+    
+    private void Awake()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    
+
+    
+}
