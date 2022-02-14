a3fd9116a700d49a7b2dd675feeb67ea45a8465d
Lo <lo.accard@gmail.com>
2022-02-14 17:32:46 +0100

    Ressource regulation

--- Mobile project/Assets/Scripts/Building/Extractor_LAC.cs
+++ Mobile project/Assets/Scripts/Building/Extractor_LAC.cs
@@ -2,20 +2,29 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Extractor_LAC : Building_LAC
+public class Extractor_LAC : Building
 {
     [Header("People")]
     [SerializeField] int[] maxPeople;
     [SerializeField] [Range(0, 1)] float peopleMultiplicator;
     public int people;
 
+    public enum RessourceType { GOLD, KNOWLEDGE }
     [Header("Product")]
+    public RessourceType ressourceType;
     [SerializeField] float[] productLevel;
     public float productCapacity;
     float productCoolDown;
 
+    [Header("Stock")]
+    public bool fonctional = true;
+    public float stock;
+
     private void Start()
     {
+        if (ressourceType == RessourceType.GOLD)
+            RessourceManager_LAC.instance.extractor_G.Add(this);
+
         UpdateProductCapacity();
     }
 
@@ -30,25 +39,43 @@ public class Extractor_LAC : Building_LAC
         
     }
 
-    new void Upgrade()
-    {
-        base.Upgrade();
-    }
+ 
 
     public void AddPeople( int people)
     {
-        int maxPeople = this.maxPeople[Mathf.Clamp(level, 0, this.maxPeople.Length)];
+        int maxPeople = this.maxPeople[Mathf.Clamp(UpgradeTier, 0, this.maxPeople.Length)];
         this.people = Mathf.Clamp(this.people + people, 1, maxPeople);
     }
 
     [ContextMenu("Update product")]
     public void UpdateProductCapacity()
     {
-        productCapacity =  productLevel[level] * (1 + (people-1) * peopleMultiplicator);   
+        productCapacity =  productLevel[UpgradeTier] * (1 + (people-1) * peopleMultiplicator);   
     }
 
     public void ProductRessource()
     {
-        RessourceManager_LAC.instance.AddGold((int)productCapacity);
+        if(ressourceType == RessourceType.GOLD)
+            RessourceManager_LAC.instance.AddGold((int)productCapacity);
+
+        if (ressourceType == RessourceType.KNOWLEDGE)
+            RessourceManager_LAC.instance.AddKnowledge(productCapacity);
+    }
+
+    [ContextMenu("Regulation Loop")]
+    public void RegulationLoop()
+    {
+        if (fonctional)
+        {
+            RessourceManager_LAC.instance.extractor_G.Remove(this);
+        }
+
+        else if (!fonctional)
+        {
+            RessourceManager_LAC.instance.extractor_G.Add(this);
+
+        }
+        fonctional = !fonctional;
+
     }
 }
--- Mobile project/Assets/Scripts/Singleton/RessourceManager_LAC.cs
+++ Mobile project/Assets/Scripts/Singleton/RessourceManager_LAC.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class RessourceManager_LAC : MonoBehaviour
 {
     public static RessourceManager_LAC instance { get; private set; }
-    public int gold;
+    public int gold; 
+    public float knowledge;
+
+    public List<Extractor_LAC> extractor_G, extractor_K;
+
     private void Awake()
     {
         // if the singleton hasn't been initialized yet
@@ -21,5 +25,26 @@ public class RessourceManager_LAC : MonoBehaviour
     public void AddGold(int value)
     {
         gold += value;
+        float globalProduct = 0;
+
+        for(int i = 0; i < extractor_G.Count; i++)
+            globalProduct += extractor_G[i].productCapacity;
+
+        for (int i = 0; i < extractor_G.Count; i++) 
+            extractor_G[i].stock +=  (float)value * extractor_G[i].productCapacity/globalProduct;
+        
     }
+
+    public void AddKnowledge(float value)
+    {
+        knowledge += value;
+        float globalProduct = 0;
+
+        for (int i = 0; i < extractor_K.Count; i++)
+            globalProduct += extractor_K[i].productCapacity;
+
+        for (int i = 0; i < extractor_K.Count; i++)
+            extractor_K[i].stock += value * extractor_K[i].productCapacity / globalProduct;
+    }
+
 }
