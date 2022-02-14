f77044535c466690ebb32ebef5b794401ac7bd01
Azzle <eliott.chauchard@orange.fr>
2022-02-14 10:34:40 +0100

    Building script

--- Mobile project/Assets/Scripts/Building.cs
+++ Mobile project/Assets/Scripts/Building.cs
@@ -6,16 +6,23 @@ using Random = UnityEngine.Random;
 
 public class Building : MonoBehaviour
 {
-    public bool[,] area;
+    public bool[,] area = new bool[2,2];
+    public GameObject Visual;
+    public int UpgradeTier;
+    public float PlacementSound;
+    public bool Activated;
 
-    /*private void Start()
+    private void Start()
     {
         for (int i = 0; i < area.GetLength(0); i++)
         {
             for (int j = 0; j < area.GetLength(1); j++)
             {
-                area[i, j] = true;
+                
+                
+                if(Random.Range(0,2) == 0) area[i, j] = true;
+                else area[i, j] = false;
             }
         }//ça c'est juste pour exemple
-    }*/
+    }
 }
