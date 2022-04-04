fa4ff3889e1d17b129e5b958893e09d59c98d059
Luac <lo.accard@gmail.com>
2022-04-04 02:40:23 +0200

    Turret WIP

--- Mobile project/Assets/Scripts/BuildingSystem/Building/Turret_LAC.cs
+++ Mobile project/Assets/Scripts/BuildingSystem/Building/Turret_LAC.cs
@@ -2,17 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Turret_LAC : MonoBehaviour
+public class Turret_LAC : Building
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    TurretSO_LAC[] stats;
+    [HideInInspector] public EnemyBoid enemyTarget;
+    public LayerMask enemyMask;
 
-    // Update is called once per frame
-    void Update()
+
+    public void UpdateTarget()
     {
-        
+
+        if (!enemyTarget)
+        {
+            float minDist = stats[level].range;
+            Transform targetT = null;
+
+            Collider[] targets = Physics.OverlapSphere(transform.position, stats[level].range, enemyMask);
+            for (int i = 0; i < targets.Length; i++)
+            {
+                if (targets[i])
+                {
+                    float dist = Vector3.Distance(transform.position, targets[i].transform.position);
+                    if(dist < minDist)
+                    {
+                        minDist = dist;
+                        targetT = targets[i].transform;
+                    }
+                }
+            }
+            enemyTarget = targetT.GetComponent<EnemyBoid>();
+        }
+        else if (Vector3.Distance(transform.position, enemyTarget.transform.position) > stats[level].range)
+            enemyTarget = null;
+    }
+    public override void Upgrade()
+    {
+        if (stats.Length <= 0)
+            return;
+        level = Mathf.Clamp(level + 1, 0, stats.Length);
     }
 }
