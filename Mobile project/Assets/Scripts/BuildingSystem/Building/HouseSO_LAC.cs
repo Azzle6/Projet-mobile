0b742ce7b1dd553497ad32458bed7c48a7d7fb76
Lo <lo.accard@gmail.com>
2022-04-04 16:18:27 +0200

    Refacto Buildings stats

--- Mobile project/Assets/Scripts/BuildingSystem/Building/HouseSO_LAC.cs
+++ Mobile project/Assets/Scripts/BuildingSystem/Building/HouseSO_LAC.cs
@@ -2,8 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(order = 2, fileName = "HouseStats", menuName = "Scriptables/BuildingSO/HouseSO")]
-public class HouseSO_LAC : ScriptableObject
+[CreateAssetMenu(order = 2, fileName = "HouseStats", menuName = "Scriptables/BuildingStat/House")]
+public class HouseSO_LAC : BuildingStatSO
 {
         public GameObject visual;
         [Header("People")]
