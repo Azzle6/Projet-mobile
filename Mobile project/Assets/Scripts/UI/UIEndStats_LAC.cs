be00c75cc1b3fa5f1f179cd9cb96b6b57ecc7541
Lo <lo.accard@gmail.com>
2022-05-25 15:55:37 +0200

    UI End

--- Mobile project/Assets/Scripts/UI/UIEndStats_LAC.cs
+++ Mobile project/Assets/Scripts/UI/UIEndStats_LAC.cs
@@ -7,18 +7,28 @@ using TMPro;
 public class UIEndStats_LAC : MonoBehaviour
 {
     public DiffcultySettings diffRef;
-    public TextMeshProUGUI playTime, enemyKilled, building, tech;
+    public GameObject victory, defeat;
+    public Animation anim;
+    [HideInInspector] public bool win;
+    [Header("Stats")]
+    public TextMeshProUGUI playTime;
+    public TextMeshProUGUI enemyKilled, building, tech;
     void Start()
     {
         EndStats_LAC.ResetStat();
+        DisplayStats(win);
     }
 
-    public void DisplayStats()
+    public void DisplayStats( bool win)
     {
+        victory.SetActive(win);
+        defeat.SetActive(!win);
+
         playTime.text = "Play Time" + Time.fixedUnscaledTime;
         enemyKilled.text = "Enemies Killed " + EndStats_LAC.enemiesKilled;
         building.text = "Buildings " + EndStats_LAC.buildings;
         tech.text = "Tech discovered " + EndStats_LAC.techDiscovered + "/" + diffRef.techMax;
+        
     }
 
 }
--- Mobile project/Assets/Scripts/UI/UIManager_LAC.cs
+++ Mobile project/Assets/Scripts/UI/UIManager_LAC.cs
@@ -62,6 +62,9 @@ public class UIManager_LAC : MonoBehaviour
     private float previousNoise = 0;
     public Animation anim_techCompleted;
 
+    [Header("End")]
+    public GameObject endCanvas;
+
     private void Awake()
     {
         InfosPannel.RegisterInstance();
@@ -441,6 +444,13 @@ public class UIManager_LAC : MonoBehaviour
         boutonMenuBatiments.SetActive(true);
     }
 
+    public void DisplayEnd()
+    {
+        if (endCanvas)
+            endCanvas.SetActive(true);
+        else
+            Debug.LogWarning("No End UI");
+    }
     #region Noise
     public void ActualizeNoiseSlider()
     {
