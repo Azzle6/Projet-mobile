ea7ddb56f4574e2e21c628f23f6e2de1004322b6
Luac <lo.accard@gmail.com>
2022-05-29 01:31:17 +0200

    Fixe Cristal repair

--- Mobile project/Assets/Scripts/UI/UIEndStats_LAC.cs
+++ Mobile project/Assets/Scripts/UI/UIEndStats_LAC.cs
@@ -9,7 +9,6 @@ public class UIEndStats_LAC : MonoBehaviour
     public DiffcultySettings diffRef;
     public GameObject victory, defeat;
     //public Animation anim;
-    [HideInInspector] public bool win;
     [Header("Stats")]
     public TextMeshProUGUI playTime;
     public TextMeshProUGUI enemyKilled, building, tech;
